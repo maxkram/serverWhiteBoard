ddf4a7723a999091f7583891793efcdaf413731e
maxkram <max.kramer@live.com>
2019-02-04 21:20:39 -0500

    with RethinkDB

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
-const io = require('socket.io')();
+const r = require("rethinkdb");
+const io = require("socket.io")();
 
-io.on('connection', (client) => {
-  client.on('subscribeToTimer', (interval) => {
-    console.log('client is subscribing to timer with interval ', interval);
-    setInterval(() => {
-      client.emit('timer', new Date());
-    }, interval);
+function createDrawing({ connection, name }) {
+  return r
+    .table("drawings")
+    .insert({
+      name,
+      timestamp: new Date()
+    })
+    .run(connection)
+    .then(() => console.log("created a new drawing with name ", name));
+}
+
+function subscribeToDrawings({ client, connection }) {
+  r.table("drawings")
+    .changes({ include_initial: true })
+    .run(connection)
+    .then(cursor => {
+      cursor.each((err, drawingRow) =>
+        client.emit("drawing", drawingRow.new_val)
+      );
+    });
+}
+
+r.connect({
+  host: "localhost",
+  port: 28015,
+  db: "awesome_whiteboard"
+}).then(connection => {
+  io.on("connection", client => {
+    client.on("createDrawing", ({ name }) => {
+      createDrawing({ connection, name });
+    });
+
+    client.on("subscribeToDrawings", () =>
+      subscribeToDrawings({
+        client,
+        connection
+      })
+    );
   });
 });
 
-const port = 8000;
+const port = 8080;
 io.listen(port);
-console.log('listening on port ', port);
-
+console.log("listening on port ", port);
